ce5574270e5df5db08ee33691fa625ce1f5c6c13
Semyon Ivanov <semeni07@mail.ru>
2023-11-24 14:24:29 +0300

    Blah-blah-blah

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from colorama import Fore, init
-from data_manager import get_data, save_entry, save_balance
+from data_manager import get_data, save_entry, delete_entry, save_balance
 
 
-def print_entries(entries):
-    print('Дата\t\tСумма')
-    for date, amount in entries.items():
-        print(f'{date}\t{amount}')
+def print_entries(entries, color=Fore.WHITE):
+    print('Дата\t\t\tКатегория\t\t\tСумма')
+    for date in entries.keys():
+        for category, amount in entries[date].items():
+            print(f'{date}\t\t{category}\t\t\t{color}{amount}')
 
 
 def get_balance():
@@ -20,32 +21,54 @@ def add_entry():
     balance = get_data()[0]['balance']
 
     user_input = input('Выберите: расходы (р) или доходы (д): ').strip().lower()
-    date_input = input('Введите дату в формате дд.мм.гггг: ').strip().lower()
-    amount_input = int(input('Введите сумму: ').strip().lower())
+    if user_input not in ('р', 'д'):
+        print(f'{Fore.RED}Неверная команда!')
+        print()
+        return
+
+    date = input('Введите дату в формате дд.мм.гггг: ').strip().lower()
+    try:
+        if not ((1 <= int(date.split('.')[0]) <= 31) and
+                (1 <= int(date.split('.')[1]) <= 12) and
+                ((0 < int(date.split('.')[2]) <= 9999))):
+            print(f'{Fore.RED}Несуществующая дата!')
+            print()
+            return
+    except:
+        print(f'{Fore.RED}Неверный формат даты!')
+        print()
+        return
+
+    amount = input('Введите сумму: ')
+    if not (amount.isdigit() and int(amount) >= 0):
+        print(f'{Fore.RED}Сумма должна быть неотрицательной!')
+        print()
+        return
+    amount = int(amount)
 
     category = input('Введите категорию: ').strip().lower()
 
     if user_input == 'р':
-        new_balance = balance - amount_input
+        new_balance = balance - amount
 
         save_balance(new_balance)
-        save_entry('expenses', date_input, amount_input, category)
+        save_entry('expenses', date, amount, category)
 
     elif user_input == 'д':
-        new_balance = balance + amount_input
+        new_balance = balance + amount
 
         save_balance(new_balance)
-        save_entry('income', date_input, amount_input, category)
+        save_entry('income', date, amount, category)
 
     else:
-        print('Неверная команда')
+        print(f'{Fore.RED}Неверная команда!')
 
-    print('Запись добавлена!')
+    print(f'{Fore.LIGHTGREEN_EX}Запись добавлена!')
     print()
 
 
 def delete_entry():
-    print('Запись удалена')
+    print('Запись удалена!')
     print()
 
 
@@ -55,19 +78,19 @@ def show_all_entries():
     user_input = input('Выберите: все записи (в), расходы (р) или доходы (д): ').strip().lower()
 
     if user_input == 'р':
-        incomes = account_data[0]['income']
-        print_entries(incomes)
+        incomes = account_data[0]['expenses']
+        print_entries(incomes, Fore.RED)
 
     elif user_input == 'д':
-        expenses = account_data[0]['expenses']
-        print_entries(expenses)
+        expenses = account_data[0]['income']
+        print_entries(expenses, Fore.GREEN)
 
     elif user_input == 'в':
-        print(account_data[0]['income'])
-        print(account_data[0]['expenses'])
+        # TODO
+        pass
 
     else:
-        print('Неверная команда')
+        print(f'{Fore.RED}Неверная команда!')
 
     print()
 
@@ -98,6 +121,10 @@ def interactions():
 
         elif user_input == 'Q':
             quit()
+
+        else:
+            print(f'{Fore.RED}Неверная команда!')
+            print()
 
 
 def main():
